95aa0491b0c9d80f74be9f34c3fab70845778d80
Neetesh Gupta <neetesh.ng@gmail.com>
2021-05-14 16:22:48 +0100

    Use ViewModel for ThumbnailView

--- Swiftuiapp/Views/ThumbnailView.swift
+++ Swiftuiapp/Views/ThumbnailView.swift
@@ -4,43 +4,50 @@ import QuickLook
 struct ThumbnailView: View {
     let url: URL
 
-    @State private var thumbnail: CGImage? = nil
+    @StateObject var viewModel = ThumbnailViewModel()
 
     var body: some View {
         Group {
-            if thumbnail != nil {
-                Image(self.thumbnail!, scale: (UIScreen.main.scale), label: Text("PDF"))
+            if let thumbnail = viewModel.thumbnail {
+                Image(thumbnail, scale: (UIScreen.main.scale), label: Text("Thumbnail"))
                     .resizable()
                     .scaledToFit()
                     .aspectRatio(contentMode: .fill)
             } else {
-                Image(systemName: "photo.fill").onAppear(perform: generateThumbnail)
+                Image(systemName: "photo.fill")
+                    .onAppear(perform: {
+                        viewModel.generateThumbnail(url)
+                    })
             }
         }
     }
+}
 
-    func generateThumbnail() {
-        let size: CGSize = CGSize(width: 400, height: 400)
-        let request = QLThumbnailGenerator.Request(fileAt: url, size: size, scale: (UIScreen.main.scale), representationTypes: .all)
-        let generator = QLThumbnailGenerator.shared
+extension ThumbnailView {
+    class ThumbnailViewModel: ObservableObject {
+        @Published var thumbnail: CGImage?
 
-        generator.generateRepresentations(for: request) { (thumbnail, type, error) in
-            DispatchQueue.main.async {
-                if thumbnail == nil || error != nil {
-                    // TODO: Default icon
-                } else {
-                    DispatchQueue.main.async {
-                        self.thumbnail = thumbnail!.cgImage
+        func generateThumbnail(_ url: URL) {
+            let size: CGSize = CGSize(width: 400, height: 400)
+            let request = QLThumbnailGenerator.Request(fileAt: url, size: size, scale: (UIScreen.main.scale), representationTypes: .all)
+            let generator = QLThumbnailGenerator.shared
+
+            generator.generateRepresentations(for: request) { (thumbImage, type, error) in
+                DispatchQueue.main.async {
+                    if thumbImage == nil || error != nil {
+                        // TODO: Default icon
+                    } else {
+                        self.thumbnail = thumbImage!.cgImage
                     }
                 }
             }
         }
     }
 }
-//
-//struct ThumbnailView_Preview: PreviewProvider {
-//    static var previews: some View {
-//        ThumbnailView(url: URL)
-//    }
-//}
-//
+
+struct ThumbnailView_Preview: PreviewProvider {
+    static var previews: some View {
+        ThumbnailView(url: URL(fileURLWithPath: ""))
+    }
+}
+
