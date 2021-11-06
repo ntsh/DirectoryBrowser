271717723094d0bfae6ba66c8dd9e9533490db08
Neetesh Gupta <neetesh.ng@gmail.com>
2021-11-06 17:38:39 +0000

    Cleanup commented out code

--- Swiftuiapp/SwiftuiappApp.swift
+++ Swiftuiapp/SwiftuiappApp.swift
@@ -6,14 +6,7 @@ struct SwiftuiappApp: App {
 
     var body: some Scene {
         WindowGroup {
-//            ContentView()
-//                .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-
-//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-
-            //  HomeView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
             HomeView(documentsStore: DocumentsStore(relativePath: "", sorting: .date(ascending: true))).environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-
         }
     }
 }
